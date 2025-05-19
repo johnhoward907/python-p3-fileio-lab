b87611d4f616d73edd0dbde02f2acc01e9769ba5
John <jjhhoward1738@gmail.com>
2025-05-19 20:46:09 +0300

    chnages

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    full_file_name = Path(file_name).with_suffix('.txt')
+    with open(full_file_name, "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    full_file_name = Path(file_name).with_suffix('.txt')
+    with open(full_file_name, "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    full_file_name = Path(file_name).with_suffix('.txt')
+    with open(full_file_name, "r") as file:
+        return file.read()
